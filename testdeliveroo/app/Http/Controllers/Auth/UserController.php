c156f6f301228995127e5127f551278ce53eb4fb
Alessandro Dongu <alessa.dongu@gmail.com>
2021-06-23 15:06:40 +0200

    done

---
+++ testdeliveroo/app/Http/Controllers/Auth/UserController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Restaurant;
+
+use Illuminate\Http\Request;
+
+Use App\Library\Helpers\MyValidation;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this -> middleware('auth') -> except(['create']);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $restaurants = Restaurant::all() ->where('user_id', $user ->id);
+
+        return view('pages.users.show', compact(
+            'user',
+            'restaurants'
+        ));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);       
+        $user -> delete();
+        return redirect() ->route('homepage');
+    }
+}
